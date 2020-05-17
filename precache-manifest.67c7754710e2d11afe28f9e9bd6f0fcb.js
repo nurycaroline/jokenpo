7851b2272907049ba75c8817cee5b4dc8b52f69a
Nurielly Caroline Brizola <nurycaroline@gmail.com>
2020-05-17 20:43:13 -0300

    Updates

---
+++ precache-manifest.67c7754710e2d11afe28f9e9bd6f0fcb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c320245aba41e4dfe130f1732a3b2d89",
+    "url": "/jokenpo/index.html"
+  },
+  {
+    "revision": "8acc73183e93af54e7de",
+    "url": "/jokenpo/static/js/2.d37ca95b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jokenpo/static/js/2.d37ca95b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7be14c382eec5da413e7",
+    "url": "/jokenpo/static/js/main.9a0a7519.chunk.js"
+  },
+  {
+    "revision": "8badfebe54ecf938ad75",
+    "url": "/jokenpo/static/js/runtime-main.e6c6e53d.js"
+  },
+  {
+    "revision": "1fd44bdf9274b331fe659004ccc71e63",
+    "url": "/jokenpo/static/media/bg-triangle.1fd44bdf.svg"
+  },
+  {
+    "revision": "919ab92a5d671f1b7fdd3e5f97eb816c",
+    "url": "/jokenpo/static/media/icon-close.919ab92a.svg"
+  },
+  {
+    "revision": "69a8c2e96b659b35a3760c79123be610",
+    "url": "/jokenpo/static/media/icon-paper.69a8c2e9.svg"
+  },
+  {
+    "revision": "8ce1e109bb43912192f5ba8dfcca005e",
+    "url": "/jokenpo/static/media/icon-rock.8ce1e109.svg"
+  },
+  {
+    "revision": "bec2f9b3d60cb80987106bb49205c53f",
+    "url": "/jokenpo/static/media/icon-scissors.bec2f9b3.svg"
+  },
+  {
+    "revision": "5d74d25d9c64bde346585ec7e7587a69",
+    "url": "/jokenpo/static/media/image-rules.5d74d25d.svg"
+  },
+  {
+    "revision": "759a207509c8dad61b7e365b27e0d0bd",
+    "url": "/jokenpo/static/media/logo.759a2075.svg"
+  }
+]);
